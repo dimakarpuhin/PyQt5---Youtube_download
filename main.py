250534b34cc809e1f261421ce56e8e629467dd18
dimakarpuhin <karpuxin.1996@mail.ru>
2024-03-21 22:20:41 +0300

    Написал все необходимые методы

--- main.py
+++ main.py
@@ -5,3 +5,107 @@ from PyQt5 import QtCore, QtGui, QtWidgest
 
 class downloader(QtCore.QThread):
     mysignal = QtCore.pyqtSignal(str)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.url = None
+
+
+    def run(self):
+        self.mysignal.emit('Процесс скачивания запущен')
+
+        with youtube_dl.YoutubeDL({}) as ydl:
+            ydl.download([self.url])
+
+        self.mysignal.emit('Процесс скачивания завершен')    
+        self.mysignal.emit('finish')  
+
+    def init_args(self, url):
+        self.url = url
+
+class gui(QtWidgest.QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui = setupUi(self)
+
+        self.download_folder = None
+        self.ui.pushButton.clicked.connect(self.get_folder)
+        self.ui.pushButton_2.cliked.connect(self.start)
+        self.mythread = downloader()
+        self.mythread.mysignal.connect(self.handler)
+
+    def start(self):
+        if len(self.ui.lineEdit.text()) > 5:
+            if self.download_folder != None:
+                link = self.ui.lineEdit.text()
+                self.mythread.init_args(link)
+                self.mythread.start()
+                self.locker(True)
+            else:
+                QtWidgest.QMessageBox.warning(self,"Ошибка", "Вы не выбрали папку!")    
+        else:
+            QtWidgest.QMessageBox.warning(self,"Ошибка", "Ссылк на видео не указана")  
+
+
+    def get_folder(self):
+        self.download_folder = QtWidgest.QFileDirectory(self,'Выбрать папку для сохранения')
+        os.chdir(self.download_folder)
+
+
+    def handler(self, value):
+        if value == 'finish':
+            self.locked(False)
+
+        self.ui.planTextEdit.appendPlainText(value)   
+
+
+    def locked(self, lock_value):
+        base = [self.ui.pushButton, self.ui.pushButton_2]
+
+        for item in base:
+            item.setDisabled(lock_value)
+
+
+
+
+if __name__ == "__main__":
+    app = QtWidgest.QApplication(sys.argv)
+    win = gui()
+    win.show()
+    sys.exit(app.exac_())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
